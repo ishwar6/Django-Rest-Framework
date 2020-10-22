3a160934b1eabd6aec72d2f8f6f44afb359612f5
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2020-10-22 16:24:25 +0530

    Main Urls File

--- urls.py
+++ urls.py
@@ -1,60 +1,35 @@
-from django.conf.urls import include, url
-from rest_framework import routers
-from knox import views as knox_views
-from .views import (
-
-            
-            UserAPI,
-           
-          
-            ChangePasswordAPI,
-           
-         
-            LoginAPI
-            
-            )
-
-from .views import ValidatePhoneSendOTP, ForgetPasswordChange, ValidateOTP, Register, ValidatePhoneForgot, ForgotValidateOTP
-
-app_name = 'accounts'
-
-# urlpatterns = [
-#     url("^", include(router.urls)),
-#     url("^auth/register/$", RegistrationAPI.as_view()),
-#     url("^auth/login/$", LoginAPI.as_view()),
-#     url("^auth/logout/$", knox_views.LogoutView.as_view()),
-#     url("^auth/logoutall/$", knox_views.LogoutAllView.as_view()),
-#     url("^auth/user/$", UserAPI.as_view()),
-#     url("^auth/validate_otp/$", validate_otp),
-#     url("^auth/validate_phone/$", validate_phone),
-#     url("^auth/change_password/$", ChangePasswordAPI.as_view()),
-#     url("^auth/reset_password/$", ResetPasswordAPI.as_view()),
-#     url("^auth/forget_password/$", ForgetPasswordAPI.as_view()),
-#     url("^auth/user_exists/$", user_existed),
-# ]
-
+from django.urls import path, include, re_path
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+admin.site.site_header = 'Wisfrags Education Private Limited - BlissedMaths SuperAdmin Panel'
+admin.site.site_title = 'Wisfrags Education'
+admin.site.index_title = 'Managed by Gaurav Malhotra, Pankaj Kumar & Ishwar Jangid'
 
 urlpatterns = [
 
-# for registering a new user
-
-    url("^auth/validate_phone/$", ValidatePhoneSendOTP.as_view()),
-    url("^auth/validate_otp/$", ValidateOTP.as_view()),
-    url("^auth/register/$", Register.as_view()),
-
-    # for forgot password
-
-    url("^auth/forgot_validate_phone/$", ValidatePhoneForgot.as_view()),
-    url("^auth/forgot_validate_otp/$", ForgotValidateOTP.as_view()),
-    url("^auth/forgot_password/$", ForgetPasswordChange.as_view()),
 
 
-    # for login and logout
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^api/', include('accounts.urls', namespace='account')),
+    re_path(r'^assess/', include('check.urls', namespace='check')),
 
-    url("^auth/login/$", LoginAPI.as_view()),
-     url("^auth/logout/$", knox_views.LogoutView.as_view()),
-    url("^auth/logoutall/$", knox_views.LogoutAllView.as_view()),
 
-    url("^auth/user/$", UserAPI.as_view()),
 
+   
 ]
+
+
+
+
+
+
+
+
+
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + \
+        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + \
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
